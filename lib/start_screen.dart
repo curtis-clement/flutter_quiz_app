51f684513fcaa18dd9ba6942bbdd37625e0cb40f
Curtis Clement <curtis.clement@wonderkind.com>
2024-08-05 09:25:25 +0200

    add image, text, and start button to start home screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({ super.key });
+  const StartScreen({super.key});
 
   @override
   Widget build(context) {
-    return const Center(child: Text('Start Screen'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/quiz-logo.png',
+            width: 250,
+          ),
+          const SizedBox(height: 50),
+          const Text(
+            'Learn Flutter with our Quiz App!',
+            style: TextStyle(fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 196, 90, 214),
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
