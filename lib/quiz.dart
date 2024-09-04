f2a8ac7d3c0c0183c8ab507178d5ae50f151097c
Curtis Clement <curtis.clement@wonderkind.com>
2024-09-04 15:42:43 +0200

    add question_screen and quiz widgets and refactor main.dart file

---
+++ lib/quiz.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [ Color.fromARGB(255, 154, 228, 157), Color.fromARGB(255, 46, 160, 49) ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
