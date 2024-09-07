2dd710a4b8eee178cdd9850b6acf7ab4c71aa96b
Curtis Clement <curtis.clement@wonderkind.com>
2024-09-07 15:49:17 +0200

    update dependencies and install google fonts

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton({ super.key, required this.answerText, required this.onPressed });
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onPressed});
 
   final String answerText;
   final void Function() onPressed;
@@ -18,7 +19,10 @@ class AnswerButton extends StatelessWidget {
         ),
         padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
       ),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
