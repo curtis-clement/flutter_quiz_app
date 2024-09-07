ba628a317782f6e94227b86b6ec9ca1f377c4095
Curtis Clement <curtis.clement@wonderkind.com>
2024-09-07 11:14:33 +0200

    display answers in a shuffled state

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -18,33 +18,28 @@ class _QuestionScreenState extends State<QuestionScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(height: 30),
-          AnswerButton(
-            answerText: currentQuestion.answers[0],
-            onPressed: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[1],
-            onPressed: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[2],
-            onPressed: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[3],
-            onPressed: () {},
-          ),
-        ],
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onPressed: () {},
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
